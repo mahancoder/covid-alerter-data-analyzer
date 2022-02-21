31264b5dab41c531bef854bff479d5d8d319d4d1
Mahan Lamee <mahan.lameie87@gmail.com>
2022-02-21 22:12:11 +0330

    Added full PAR calculation code

--- model.py
+++ model.py
@@ -1,20 +1,28 @@
+"""The database model"""
 from sqlalchemy.sql.schema import ForeignKey
-from sqlalchemy.sql.sqltypes import Boolean, DateTime, Integer, String, Numeric
-import sqlalchemy, sqlalchemy.orm
+from sqlalchemy.sql.sqltypes import Boolean, DateTime, Integer, String, Numeric, Float
+import sqlalchemy
+import sqlalchemy.orm
 from sqlalchemy.orm import relationship
-from sqlalchemy.ext.declarative import declarative_base 
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class Report(Base):
+    """The reports table model"""
     __tablename__ = "Reports"
     Id = sqlalchemy.Column(Integer, primary_key=True)
     Longitude = sqlalchemy.Column(Numeric)
     Latitude = sqlalchemy.Column(Numeric)
     UserId = sqlalchemy.Column(Integer)
-    NeighbourhoodId = sqlalchemy.Column(Integer, ForeignKey("Neighbourhoods.Id"))
+    NeighbourhoodId = sqlalchemy.Column(
+        Integer, ForeignKey("Neighbourhoods.Id"))
     Neighbourhood = relationship("Neighbourhood", back_populates="Reports")
+
+
 class User(Base):
+    """The users table model"""
     __tablename__ = "Users"
     Id = sqlalchemy.Column(Integer, primary_key=True)
     LastLocationId = sqlalchemy.Column(Integer)
@@ -22,16 +30,34 @@ class User(Base):
     LastInteraction = sqlalchemy.Column(DateTime)
     GoogleId = sqlalchemy.Column(String)
     SessionId = sqlalchemy.Column(String)
-    LastLocationId = sqlalchemy.Column(Integer, ForeignKey("Neighbourhoods.Id"))
+    LastLocationId = sqlalchemy.Column(
+        Integer, ForeignKey("Neighbourhoods.Id"))
     LastLocation = relationship("Neighbourhood", back_populates="Users")
+
+
+class ChildParents(Base):
+    """The neighbourhood childs and parents relationship table"""
+    __tablename__ = "ChildParents"
+    Id = sqlalchemy.Column(Integer, primary_key=True, autoincrement=True)
+    ParentsId = sqlalchemy.Column(Integer, ForeignKey("Neighbourhoods.Id"))
+    ChildsId = sqlalchemy.Column(Integer, ForeignKey("Neighbourhoods.Id"))
+
+
 class Neighbourhood(Base):
+    """The neighbourhoods table model"""
     __tablename__ = "Neighbourhoods"
-    Id = sqlalchemy.Column(Integer, primary_key=True)
-    Ratio = sqlalchemy.Column(Integer)
-    HasChilds= sqlalchemy.Column(Boolean)
+    Id = sqlalchemy.Column(Integer, primary_key=True, autoincrement=True)
+    Ratio = sqlalchemy.Column(Float)
+    HasChilds = sqlalchemy.Column(Boolean)
     LiveCount = sqlalchemy.Column(Integer)
     Name = sqlalchemy.Column(String)
     OSMId = sqlalchemy.Column(String)
-    OSMType = sqlalchemy.Column(String)
+    IsRelation = sqlalchemy.Column(Boolean)
+    IsBig = sqlalchemy.Column(Boolean)
     Users = relationship("User", back_populates="LastLocation")
     Reports = relationship("Report", back_populates="Neighbourhood")
+    # Parents = relationship("Neighbourhood", back_populates="Childs",
+    #    secondary="ChildParents", foreign_keys=ChildParents.ParentsId)
+    Childs = relationship("Neighbourhood", backref="Parents",
+                          secondary="ChildParents", primaryjoin=Id == ChildParents.ParentsId,
+                          secondaryjoin=Id == ChildParents.ChildsId)
